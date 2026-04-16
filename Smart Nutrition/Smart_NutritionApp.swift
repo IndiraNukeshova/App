a152ed4f2a0bb1ddd9b88bb3da5c32062c9eec42
_indi13_ <nukeshova13@mail.ru>
2026-04-16 17:38:01 +0300

    Initial Commit

---
+++ Smart Nutrition/Smart_NutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_NutritionApp.swift
+//  Smart Nutrition
+//
+//  Created by nukeshova13 on 16.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Smart_NutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart NutritionTests/Smart_NutritionTests.swift
@@ -0,0 +1,19 @@
+//
+//  Smart_NutritionTests.swift
+//  Smart NutritionTests
+//
+//  Created by nukeshova13 on 16.04.2026.
+//
+
+import Testing
+@testable import Smart_Nutrition
+
+struct Smart_NutritionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
